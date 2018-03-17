2b9abe62513b708b58e40010b80b633732b71674
ana maria <ana.sanchez2@udea.edu.co>
2018-03-17 11:37:35 -0500

    Creacion del proyecto

---
+++ ongconcordia/ongconcordia/Controllers/PacienteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ongconcordia.Controllers
+{
+    public class PacienteController : Controller
+    {
+        //
+        // GET: /Paciente/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Paciente/Crear
+        public ActionResult Crear()
+        {
+            return View();
+        }
+	
+	}
+}
